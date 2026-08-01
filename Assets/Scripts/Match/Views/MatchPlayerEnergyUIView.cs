8d710857b2e3a7e805fa0c88bebc9efdd76cc8ed
Lupanar <lupanar69@tutamail.com>
2026-08-01 17:40:18 +0200

    màj

---
+++ Assets/Scripts/Match/Views/MatchPlayerEnergyUIView.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Match
+{
+    /// <summary>
+    /// Affiche la barre d'énergie du joueur
+    /// </summary>
+    public class MatchPlayerEnergyUIView : MonoBehaviour
+    {
+        #region Inspecteur
+
+        /// <summary>
+        /// L'objet contenant l'UI de la barre d'énergie
+        /// </summary>
+        private Transform _playerEnegyBarParent;
+
+        #endregion
+    }
+}
